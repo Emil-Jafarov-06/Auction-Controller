a2621548f24b3d8b9d60708434b633edf6855c3e
Emil Jafarov <EmilJafarov3841@gmail.com>
2026-06-22 09:51:54 +0400

    Swagger configuration set

---
+++ src/main/java/com/team4/auctioncontroller/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.team4.auctioncontroller.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI auctionControllerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auction Controller API")
+                        .description("API documentation for Auction, Bid, and Bidder operations.")
+                        .version("1.0.0")
+                        .contact(new Contact().email("EmilJafarov3841@gmail.com")
+                                .name("Emil Jafarov")));
+    }
+
+}
