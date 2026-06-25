8abc81332d3ab74996482b9034f8e4c84cd1f7d0
Emil Jafarov <EmilJafarov3841@gmail.com>
2026-06-25 15:47:39 +0400

    The initial version of api-gateway for the project is set

---
+++ api-gateway/src/main/java/com/project/apigateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.project.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange.pathMatchers("/api/auth/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
+                ).build();
+    }
+
+}
